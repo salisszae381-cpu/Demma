284cfd91349dc2d3a396aee64942f11c80a10f44
salisszae381-cpu <salisszae381@gmail.com>
2026-09-14 06:35:44 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://lpizyydqhxxnyvxhqbnj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwaXp5eWRxaHh4bnl2eGhxYm5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMDY0NDgsImV4cCI6MjEwNDc4MjQ0OH0.GVmzLzaeCRPQ609mSde9AuTeuiUPm0eEYZkMKp2SXEA';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
